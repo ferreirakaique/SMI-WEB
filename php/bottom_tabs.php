46ed84cac6a2751390e25639fca7f5670e1b269b
Kaique Ferreira <kaique1245br@gmail.com>
2025-08-27 21:00:33 -0300

    First Commit - Bottom tabs em processo

---
+++ php/bottom_tabs.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link flex href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="../css/bottom_tabs.css">
+    <script src="../js/bottom_tabs.js" defer></script>
+</head>
+
+<body>
+    <div id="bottom_tabs">
+        <ul>
+            <li class='list'>
+                <a href="inicio.php">
+                    <span class='icon'>
+                        <i class="bx bx-home-alt"></i>
+                    </span>
+                    <span class='text'>Início</span>
+                </a>
+            </li>
+            <li class='list'>
+                <a href="listar_maquinas.php">
+                    <span class='icon'>
+                        <i class="bx bx-list-ul"></i>
+                    </span>
+                    <span class='text'>Maquinas</span>
+                </a>
+            </li>
+            <li class='list'>
+                <a href="qrcode.php">
+                    <span class='icon'>
+                        <i class="bx bx-qr"></i>
+                    </span>
+                    <span class='text'>QR Code</span>
+                </a>
+            </li>
+            <li class='list'>
+                <a href="notification.php">
+                    <span class='icon'>
+                        <i class="bx bx-bell"></i>
+                    </span>
+                    <span class='text'>Notificacoes</span>
+                </a>
+            </li>
+            <li class='list'>
+                <a href="perfil.php">
+                    <span class='icon'>
+                        <i class="bx bx-user"></i>
+                    </span>
+                    <span class='text'>Perfil</span>
+                </a>
+            </li>
+            <div class='list'></div>
+        </ul>
+        </header>
+
+</body>
+
+</html>
---
+++ php/perfil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link flex href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="../css/perfil.css">
+    <script src="../js/perfil.js" defer></script>
+    <title>Perfil</title>
+</head>
+
+<body>
+    <?php include 'bottom_tabs.php'; ?>
+
+    <main>
+        <div class="logo">
+            <h1>Perfil</h1>
+            <img src="../img/SMI_LOGO.png" alt="">
+        </div>
+    </main>
+</body>
+
+</html>
